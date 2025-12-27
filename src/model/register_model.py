12958573ba3761579ab185e980e751de7066dd5e
aashu <157953935+aashu-0@users.noreply.github.com>
2025-12-27 17:05:24 +0530

    daghub creds added

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -14,26 +14,26 @@ warnings.filterwarnings("ignore")
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("DASGHUB_TOKEN")
+if not dagshub_token:
+    raise EnvironmentError("DASGHUB_TOKEN environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "aashu-0"
+repo_name = "MLOps_Learning_Project"
 
 # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/aashu-0/MLOps_Learning_Project.mlflow')
-dagshub.init(repo_owner='aashu-0', repo_name='MLOps_Learning_Project', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/aashu-0/MLOps_Learning_Project.mlflow')
+# dagshub.init(repo_owner='aashu-0', repo_name='MLOps_Learning_Project', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
